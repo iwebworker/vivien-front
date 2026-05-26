fbb6f19633c52817f0528360e802030c247dcbf1
iwebworker <marat.webworker@mail.ru>
2026-05-27 01:16:04 +0300

    fix

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -73,7 +73,7 @@
               v-model="catalogStore.filters.search"
               type="text"
               placeholder="Поиск товаров..."
-              class="w-full pl-10 pr-4 py-2.5 text-light-text dark:text-white bg-light-bg dark:bg-white border border-light-border dark:border-purple-400/50 rounded-xl focus:border-purple-400/50 focus:ring-2 focus:ring-purple-400/20"
+              class="w-full pl-10 pr-4 py-2.5 text-black dark:text-black bg-light-bg dark:bg-white border border-light-border dark:border-purple-400/50 rounded-xl focus:border-purple-400/50 focus:ring-2 focus:ring-purple-400/20"
           />
           <svg class="w-5 h-5 absolute left-3 top-3 text-light-text-secondary dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
